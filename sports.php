3e575e0b912fdc6590b45c56db3ef51408a2d74e
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-08 13:55:44 +0600

    Create sports.php

---
+++ sports.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Sports Website</title>	<link rel="stylesheet" type="text/css" href="sports.css">
+
+</head>
+
+<body>
+
+	<header>
+
+		<nav>
+
+			<ul>
+
+				<li><a href="index.php">Home</a></li>
+
+				<li><a href="world.php">World</a></li>
+
+				<li><a href="sports.php">Sports</a></li>
+
+				<li><a href="technology.php">Technology</a></li>
+
+				
+
+			</ul>
+
+		</nav>
+
+	</header>
+
+	
+
+	<section class="banner">
+
+		<h1>Welcome to Sports Website</h1>
+
+	</section>
+
+	
+
+	<section class="content">
+
+		<h2>Latest News</h2>
+
+		<div class="news">
+
+			<h3>Team Wins Championship Title</h3>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel sapien eu massa suscipit varius. Etiam vitae nulla sapien. Ut sed justo lorem. Nam tristique libero vel dolor tempor, et dictum nisl laoreet.</p>
+
+		</div>
+
+		<div class="news">
+
+			<h3>Player Breaks Record</h3>
+
+			<p>Nullam tempus lacus risus, vel vulputate turpis bibendum sed. Praesent gravida tincidunt purus, non tincidunt mi scelerisque non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed mollis.</p>
+
+		</div>
+
+	</section>
+
+	
+
+	<footer>
+
+		<p>&copy; 2023 ABC News BD. All rights reserved.</p>
+
+	</footer>
+
+</body>
+
+</html>
